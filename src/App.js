099daff7a4fcc1ed0f837979bff7b6e3e3c68cda
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-04-25 10:59:33 -0400

    ButtonGroup and ToggleButton MUI

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
   return (
     <div className="App">
       <h2>MUI 5 Typography variants</h2>
+
       <MuiTypo />
+      <h2>MUI 5 Button variants</h2>
       <MuiButton />
     </div>
   );
--- src/components/MuiButton.js
+++ src/components/MuiButton.js
@@ -1,10 +1,27 @@
-import React from "react";
+import { useState } from "react";
 import Button from "@mui/material/Button";
-import { IconButton, Stack } from "@mui/material";
+import {
+  ButtonGroup,
+  IconButton,
+  Stack,
+  ToggleButton,
+  ToggleButtonGroup,
+  Typography,
+} from "@mui/material";
 
 import SendIcon from "@mui/icons-material/Send";
+import FormatBoldIcon from "@mui/icons-material/FormatBold";
+import FormatItalicIcon from "@mui/icons-material/FormatItalic";
+import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
 
 function MuiButton() {
+  const [formats, setFormats] = useState([]);
+
+  const handleFormatChange = (e, newFormats) => {
+    setFormats(newFormats);
+  };
+
+  console.log(formats);
   return (
     <>
       <Stack spacing={4} direction="row" mb={3}>
@@ -29,33 +46,32 @@ function MuiButton() {
       </Stack>
       <Stack spacing={2} direction="row">
         <Button variant="text" color="primary">
-          button
+          text
         </Button>
         <Button variant="contained" color="warning">
-          button
+          contained
         </Button>
         <Button variant="outlined" color="success">
-          button
+          outlined
         </Button>
       </Stack>
-
       <Stack display="block" spacing={4} direction="row" mt={3}>
         <Button variant="contained" size="small">
-          BUTTON
+          small
         </Button>
         <Button variant="contained" size="medium">
-          BUTTON
+          medium
         </Button>
         <Button variant="contained" size="large">
-          BUTTON
+          large
         </Button>
       </Stack>
       <Stack spacing={2} direction="row" mt={3}>
         <Button variant="contained" disableRipple startIcon={<SendIcon />}>
-          Button
+          Send
         </Button>
         <Button variant="contained" disableElevation endIcon={<SendIcon />}>
-          Button
+          Send
         </Button>
         <IconButton
           color="success"
@@ -64,6 +80,46 @@ function MuiButton() {
         >
           <SendIcon />
         </IconButton>
+      </Stack>
+      <Stack direction="row" mt={3}>
+        <ButtonGroup variant="outlined">
+          <Button>Left</Button>
+          <Button>Center</Button>
+          <Button>Right</Button>
+        </ButtonGroup>
+      </Stack>
+      <Stack mt={3}>
+        <ButtonGroup
+          variant="contained"
+          orientation="vertical"
+          size="small"
+          color="success"
+        >
+          <Button>Upper</Button>
+          <Button>Center</Button>
+          <Button>Lower</Button>
+        </ButtonGroup>
+      </Stack>
+      <Typography variant="h5" color="initial" mt={4}>
+        Toggle Button
+      </Typography>
+      <Stack direction="row" mt={3}>
+        <ToggleButtonGroup
+          value={formats}
+          onChange={handleFormatChange}
+          size="samll"
+          color="success"
+        >
+          <ToggleButton value="bold" aria-label="bold">
+            <FormatBoldIcon />
+          </ToggleButton>
+          <ToggleButton value="italic" aria-label="italic">
+            <FormatItalicIcon />
+          </ToggleButton>
+          <ToggleButton value="underlined" aria-label="underlined">
+            <FormatUnderlinedIcon />
+          </ToggleButton>
+        </ToggleButtonGroup>
       </Stack>
     </>
   );
