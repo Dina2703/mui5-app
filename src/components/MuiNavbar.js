2c34cf63acddb715980ec4b4b20787a9d06d2eb6
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-04-27 09:04:13 -0400

    Menu MUI

--- src/components/MuiNavbar.js
+++ src/components/MuiNavbar.js
@@ -3,13 +3,30 @@ import {
   AppBar,
   Button,
   IconButton,
+  Menu,
+  MenuItem,
   Stack,
   Toolbar,
   Typography,
 } from "@mui/material";
-import React from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import React, { useState } from "react";
 
 function MuiNavbar() {
+  const anchorRef = React.useRef(null);
+  const [open, setOpen] = useState(false);
+
+  const handleClick = (e) => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (e) => {
+    if (anchorRef.current && anchorRef.current.contains(e.target)) {
+      return;
+    }
+    setOpen(false);
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
@@ -23,8 +40,34 @@ function MuiNavbar() {
           <Button color="inherit">Features</Button>
           <Button color="inherit">Pricing</Button>
           <Button color="inherit">About</Button>
+          <Button
+            color="inherit"
+            id="resources-button"
+            ref={anchorRef}
+            onClick={handleClick}
+            endIcon={<KeyboardArrowDownIcon></KeyboardArrowDownIcon>}
+            aria-controls={open ? "resources-menu" : "undefined"}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+          >
+            Resource
+          </Button>
           <Button color="inherit">Login</Button>
         </Stack>
+        <Menu
+          anchorEl={anchorRef.current}
+          id="resources-menu"
+          open={open}
+          MenuListProps={{
+            "aria-labelledby": "resources-button",
+          }}
+          onClose={handleClose}
+          anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+          // transformOrigin={{ vertical: "top", horizontal: "right" }}
+        >
+          <MenuItem onClick={handleClose}>Blog</MenuItem>
+          <MenuItem onClick={handleClose}>Podcast</MenuItem>
+        </Menu>
       </Toolbar>
     </AppBar>
   );
